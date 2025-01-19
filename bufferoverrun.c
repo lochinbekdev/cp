75379c40c66b2d242e8b5e994d650c9b4df23326
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2025-01-19 08:01:33 +0500

    buffer over run

---
+++ bufferoverrun.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 10
+
+
+char buffer[BUFFER_SIZE] = "message";
+char password[BUFFER_SIZE]= "password";
+
+
+int main (char argc, char *argv) {
+    if(argc != 3) {
+        printf("Usage: %s  <password>  <string_to_string>\n", argv[0]);        
+        exit(1);
+    }
+
+    strcpy(buffer, argv[2]);
+    if(strcmp(argv[1], password) == 0) {
+        printf("password checks out\n");
+        printf("MSG: %s\n", buffer);
+    }else{
+        printf("password does not match\n");
+    }
+}
